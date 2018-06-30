a71de3032e29de936dddc4a4f7b1d79238ee66f5
CHICHEN\Chichen <Rosales.G793036@stu.sanjac.edu>
2018-06-30 00:15:19 -0500

    shopping cart changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import AppNavbar from './components/AppNavbar'
+import Shoppinglist from './components/Shoppinglist';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppNavbar />
+        <Shoppinglist />
       </div>
     );
   }

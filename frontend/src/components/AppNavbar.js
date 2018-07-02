09a4e1e9f569ccef1b2627193a44fe6afa8670ec
CISDNET\grosales <rosales.g793036@stu.sanjac.edu>
2018-07-02 11:33:36 -0500

    Finished

--- frontend/src/components/AppNavbar.js
+++ frontend/src/components/AppNavbar.js
@@ -1,37 +1,47 @@
-import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, Container, NavLink } from 'reactstrap';
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Container,
+  NavLink
+} from "reactstrap";
 
 export class AppNavbar extends Component {
-    state = {
-        isOpen: false
-    }
+  state = {
+    isOpen: false
+  };
 
-    toggle = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    };
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
 
-    render() {
-        return (
-            <div>
-                <Navbar color="dark" dark expand="sm" className="mb-5">
-                    <Container>
-                        <NavbarBrand href="/">Shopping List</NavbarBrand>
-                        <NavbarToggler onClick={this.toggle} />
-                        <Collapse isOpen={this.state.isOpen} navbar>
-                            <Nav className="ml-auto" navbar>
-                                <NavItem>
-                                    <NavLink href="/">Link 1</NavLink>
-                                    <NavLink href="/">Link 2</NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                    </Container>
-                </Navbar>
-            </div>
-        )
-    }
-};
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">Shopping List</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/GabeDesigns">
+                    Github
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
 
 export default AppNavbar;
--- frontend/src/components/ItemModal.js
+++ frontend/src/components/ItemModal.js
@@ -1,83 +1,86 @@
-import React, { Component } from 'react';
-import { Form, Label, Input, Button, Modal, ModalHeader, ModalBody, FormGroup } from 'reactstrap';
-import { connect } from 'react-redux';
-import { addItem } from '../actions/itemActions';
-import uuid from 'uuid';
+import React, { Component } from "react";
+import {
+  Form,
+  Label,
+  Input,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup
+} from "reactstrap";
+import { connect } from "react-redux";
+import { addItem } from "../actions/itemActions";
 
 class ItemModal extends Component {
+  state = {
+    modal: false,
+    name: ""
+  };
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  };
 
-    state = {
-        modal: false,
-        name: ''
-    }
-    toggle = () => {
-        this.setState({
-            modal: !this.state.modal
-        })
-    }
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    onChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value });
-    }
+  onSubmit = e => {
+    e.preventDefault();
 
-    onSubmit = e => {
-        e.preventDefault();
+    const newItem = {
+      name: this.state.name
+    };
+    //Add item via action
+    this.props.addItem(newItem);
 
-        const newItem = {
-            id: uuid(),
-            name: this.state.name
-        }
-        //Add item via action
-        this.props.addItem(newItem);
+    //close modal
+    this.toggle();
+  };
 
-        //close modal
-        this.toggle();
+  render() {
+    return (
+      <div>
+        <Button
+          color="dark"
+          style={{ marginBottom: "2rem" }}
+          onClick={this.toggle}
+        >
+          Add Item
+        </Button>
 
-    }
-
-    render() {
-        return (
-            <div>
-                <Button
-                    color="dark"
-                    style={{ marginBottom: '2rem' }}
-                    onClick={this.toggle}>Add Item
-            </Button>
-
-                <Modal
-                    isOpen={this.state.modal}
-                    toggle={this.toggle}
-                >
-                    <ModalHeader toggle={this.toggle}>Add To Shopping List</ModalHeader>
-                    <ModalBody>
-                        <Form onSubmit={this.onSubmit}>
-                            <FormGroup>
-                                <Label for="item">Item</Label>
-                                <Input
-                                    type="text"
-                                    name="name"
-                                    id="item"
-                                    placeholder="Add Shopping Item"
-                                    onChange={this.onChange}
-                                />
-                                <Button
-                                    color="dark"
-                                    style={{ marginTop: '2rem' }}
-                                    block
-                                >Add Item</Button>
-                            </FormGroup>
-                        </Form>
-                    </ModalBody>
-                </Modal>
-            </div >
-
-        )
-    }
-
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>Add To Shopping List</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <Label for="item">Item</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  id="item"
+                  placeholder="Add Shopping Item"
+                  onChange={this.onChange}
+                />
+                <Button color="dark" style={{ marginTop: "2rem" }} block>
+                  Add Item
+                </Button>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => ({
-    item: state.item
+const mapStateToProps = state => ({
+  item: state.item
 });
 
-export default connect(mapStateToProps, { addItem })(ItemModal);
+export default connect(
+  mapStateToProps,
+  { addItem }
+)(ItemModal);

398880c4eb54d3ce1ceb630fbd8f6b02e273397f
CHICHEN\Chichen <Rosales.G793036@stu.sanjac.edu>
2018-07-01 23:03:14 -0500

    front end logic complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import { Container } from 'reactstrap';
 import AppNavbar from './components/AppNavbar'
 import Shoppinglist from './components/Shoppinglist';
-
+import ItemModal from './components/ItemModal';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -14,7 +15,10 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <AppNavbar />
-          <Shoppinglist />
+          <Container>
+            <ItemModal />
+            <Shoppinglist />
+          </Container>
         </div>
       </Provider>
     );
